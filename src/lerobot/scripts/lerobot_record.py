783e6b9017ae003f02774f13be0cc8481fa4fbdd
Aadi0032007 <ms.adityaraj@gmail.com>
2026-01-08 04:49:00 +0530

    debug record

--- src/lerobot/scripts/lerobot_record.py
+++ src/lerobot/scripts/lerobot_record.py
@@ -472,7 +472,7 @@ def record(cfg: RecordConfig) -> LeRobotDataset:
                     robot_action_processor=robot_action_processor,
                     robot_observation_processor=robot_observation_processor,
                     teleop=teleop,
-                    control_time_s=cfg.dataset.reset_time_s,
+                    control_time_s=None,
                     single_task=cfg.dataset.single_task,
                     display_data=cfg.display_data,
                 )
