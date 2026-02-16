d0e0bfbe3210fef82c96fda7b87a58acd6ade80c
Aadi0032007 <ms.adityaraj@gmail.com>
2026-02-16 15:28:21 +0530

    widowx robot

---
+++ src/lerobot/robots/widowx_follower/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 12:24:43 2026
+
+@author: Aadi
+"""
+
+from .config_widowx_follower import WidowXFollowerConfig
+from .widowx_follower import WidowXFollower
