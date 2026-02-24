b3ae57448d9e80b6dddbd60fca4d9bf97d06d766
Aadi0032007 <ms.adityaraj@gmail.com>
2026-02-24 15:04:34 +0530

    init nero

--- src/lerobot/robots/agx_nero_follower/__init__.py
+++ src/lerobot/robots/agx_nero_follower/__init__.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 14:57:52 2026
+
+@author: Aadi
+"""
+
 from .agx_nero_follower import AgxNeroFollower
 from .config_agx_nero_follower import AgxNeroFollowerConfig
-
-__all__ = ["AgxNeroFollower", "AgxNeroFollowerConfig"]
