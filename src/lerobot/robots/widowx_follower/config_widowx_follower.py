afbdd5b57a8a256e0f0743f251c49bf2bf121f70
Aadi0032007 <ms.adityaraj@gmail.com>
2026-04-16 20:30:27 +0530

    config with relative angle

--- src/lerobot/robots/widowx_follower/config_widowx_follower.py
+++ src/lerobot/robots/widowx_follower/config_widowx_follower.py
@@ -18,12 +18,23 @@ class WidowXFollowerConfig(RobotConfig):
     # Port to connect to the arm
     port: str
 
-    disable_torque_on_disconnect: bool = True
+    disable_torque_on_disconnect: bool = False
 
     # `max_relative_target` limits the magnitude of the relative positional target vector for safety purposes.
     # Set this to a positive scalar to have the same value for all motors, or a dictionary that maps motor
     # names to the max_relative_target value for that motor.
-    max_relative_target: float | dict[str, float] | None = None
+    max_relative_target: float | dict[str, float] | None = field(
+        default_factory=lambda: {
+            "shoulder_pan": 8.0,
+            "shoulder_lift": 12.0,
+            "shoulder_lift_opp": 12.0,  # Added
+            "elbow_flex": 8.0,
+            "elbow_flex_opp": 8.0,     # Added
+            "wrist_flex": 8.0,
+            "wrist_roll": 8.0,
+            "gripper": 25.0,            
+        }
+    )
 
     # cameras
     cameras: dict[str, CameraConfig] = field(default_factory=dict)
